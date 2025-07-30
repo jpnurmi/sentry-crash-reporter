2f7de419e759411cc2234a66b8d0b926c6f84d85
J-P Nurmi <jpnurmi@gmail.com>
2025-07-30 18:40:53 +0200

    Add Name/Email/Description input fields & Send/Cancel buttons

--- Sentry.CrashReporter/MainPage.cs
+++ Sentry.CrashReporter/MainPage.cs
@@ -1,17 +1,60 @@
 namespace Sentry.CrashReporter;
 
+using Sentry.CrashReporter.Models;
+
 public sealed partial class MainPage : Page
 {
     public MainPage()
     {
-        this
+        var viewModel = new MainPageViewModel();
+        this.DataContext(viewModel)
             .Background(ThemeResource.Get<Brush>("ApplicationPageBackgroundThemeBrush"))
-            .Content(new StackPanel()
-                .VerticalAlignment(VerticalAlignment.Center)
-                .HorizontalAlignment(HorizontalAlignment.Center)
-                .Children(
-                    new TextBlock()
-                        .Text("Hello Uno Platform!")
-                ));
+            .Content(new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = GridLength.Auto },
+                    new RowDefinition { Height = GridLength.Auto },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }
+                },
+                Children =
+                {
+                    new StackPanel
+                    {
+                        Orientation = Orientation.Vertical,
+                        Children =
+                        {
+                            new TextBlock { Text = "Name" },
+                            new TextBox()
+                                .Text(x => x.Binding(() => viewModel.Name).TwoWay()),
+                            new TextBlock { Text = "Email" },
+                            new TextBox()
+                                .Text(x => x.Binding(() => viewModel.Email).TwoWay()),
+                            new TextBlock { Text = "Description" },
+                            new TextBox
+                                {
+                                    AcceptsReturn = true,
+                                    Height = 100,
+                                    TextWrapping = TextWrapping.Wrap
+                                } 
+                                .Text(x => x.Binding(() => viewModel.Description).TwoWay()),
+                        },
+                    }.Grid(row: 0),
+
+                    new StackPanel
+                    {
+                        Orientation = Orientation.Horizontal,
+                        HorizontalAlignment = HorizontalAlignment.Right,
+                        Spacing = 8,
+                        Children =
+                        {
+                            new Button { Content = "Send" }
+                                .Command(() => viewModel.SendCommand),
+                            new Button { Content = "Cancel" }
+                                .Command(() => viewModel.CancelCommand)
+                        }
+                    }.Grid(row: 1)
+                }
+            });
     }
 }
---
+++ Sentry.CrashReporter/Models/MainPageViewModel.cs
@@ -0,0 +1,54 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Sentry.CrashReporter.Models;
+
+public class MainPageViewModel : INotifyPropertyChanged
+{
+    private string _name = string.Empty;
+    private string _email = string.Empty;
+    private string _description = string.Empty;
+
+    public string Name
+    {
+        get => _name;
+        set { _name = value; OnPropertyChanged(); }
+    }
+
+    public string Email
+    {
+        get => _email;
+        set { _email = value; OnPropertyChanged(); }
+    }
+
+    public string Description
+    {
+        get => _description;
+        set { _description = value; OnPropertyChanged(); }
+    }
+
+    public ICommand SendCommand { get; }
+    public ICommand CancelCommand { get; }
+
+    public MainPageViewModel()
+    {
+        SendCommand = new RelayCommand(_ => Send(), _ => CanSend());
+        CancelCommand = new RelayCommand(_ => Cancel());
+    }
+
+    private void Send()
+    {
+        // TODO
+    }
+
+    private bool CanSend() => !string.IsNullOrWhiteSpace(Name) && !string.IsNullOrWhiteSpace(Email);
+
+    private void Cancel()
+    {
+        // TODO
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
